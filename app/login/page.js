2055832977fd49095a09fa4508d0d757cb963406
Nora Ganbarova <88088732+ganbnuray@users.noreply.github.com>
2024-08-14 20:32:20 +0400

    remove getAuth

--- app/login/page.js
+++ app/login/page.js
@@ -78,7 +78,6 @@ export default function Signup() {
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      const auth = getAuth();
       onAuthStateChanged(auth, (user) => {
         if (user) {
           // User is signed in, store UID in sessionStorage
